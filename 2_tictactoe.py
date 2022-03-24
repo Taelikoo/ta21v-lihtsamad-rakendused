18e62a2d0009d146f30190e05cfcbfb6227a5ca5
Taelikoo <taissiaa@mail.ru>
2022-03-24 11:06:58 +0200

    Create 2_tictactoe.py

---
+++ 2_tictactoe.py
@@ -0,0 +1,46 @@
+next_position = input("Please type what position: ")
+
+if next_positio == "1":
+    print("x . .")
+    print(". . .")
+    print(". . .")
+
+if next_positio == "2":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_positio == "3":
+    print(". . x")
+    print(". . .")
+    print(". . .")
+
+if next_positio == "4":
+    print(". . .")
+    print("x . .")
+    print(". . .")
+
+if next_positio == "5":
+    print(". . .")
+    print(". x .")
+    print(". . .")
+
+if next_positio == "6":
+    print(". . .")
+    print(". . x")
+    print(". . .")
+
+if next_positio == "7":
+    print(". . .")
+    print(". . .")
+    print("x . .")
+
+if next_positio == "8":
+    print(". . .")
+    print(". . .")
+    print(". x .")
+
+if next_positio == "9":
+    print(". . .")
+    print(". . .")
+    print(". . x")
