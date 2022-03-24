d7e3fce5a26a2add0631fc6df7e5dac132277d7a
Taelikoo <taissiaa@mail.ru>
2022-03-24 11:26:06 +0200

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -1,49 +1,59 @@
 next_position = input("Please type what position: ")
 
-if next_positio == "1":
+if next_position == "1":
     print("x . .")
     print(". . .")
     print(". . .")
+
+next_position = input("Please type what position: ")
 
 if next_position == "1":
     print ("CANT DO THE MOVE")
 
-elif next_positio == "2":
-    print(". x .")
+elif next_position == "2":
+    print("x o .")
     print(". . .")
     print(". . .")
 
-elif next_positio == "3":
+    next_position = input("Please type what position: ")
+
+if next_position == "1":
+    print ("CANT DO THE MOVE")
+
+if next_position == "2":
+    print ("CANT DO THE MOVE") 
+
+elif next_position == "3":
     print(". . x")
     print(". . .")
     print(". . .")
 
-elif next_positio == "4":
+elif next_position == "4":
     print(". . .")
     print("x . .")
     print(". . .")
 
-elif next_positio == "5":
+elif next_position == "5":
     print(". . .")
     print(". x .")
     print(". . .")
 
-elif next_positio == "6":
+elif next_position == "6":
     print(". . .")
     print(". . x")
     print(". . .")
 
-elif next_positio == "7":
+elif next_position == "7":
     print(". . .")
     print(". . .")
     print("x . .")
 
-elif next_positio == "8":
+elif next_position == "8":
     print(". . .")
     print(". . .")
     print(". x .")
 
-elif next_positio == "9":
+elif next_position == "9":
     print(". . .")
     print(". . .")
     print(". . x")
