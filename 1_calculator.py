e2fa7ce52790e366fbe1a6aed6982c411b40fca5
Taelikoo <taissiaa@mail.ru>
2022-04-07 08:02:53 +0300

    Create 1_calculator.py

---
+++ 1_calculator.py
@@ -0,0 +1,35 @@
+operation = input ("pick action: +, -, /, //, %, *, **: ")
+if operation == "+": 
+    n1 = int(input("enter first number: "))
+    n2 = int(input("enter second number: "))
+    print(n1 + n2)
+
+if operation == "-": 
+    n1 = int(input("enter first number: "))
+    n2 = int(input("enter second number: "))
+    print(n1 - n2)
+
+if operation == "/": 
+    n1 = int(input("enter first number: "))
+    n2 = int(input("enter second number: "))
+    print(n1 / n2)
+
+if operation == "//": 
+    n1 = int(input("enter first number: "))
+    n2 = int(input("enter second number: "))
+    print(n1 // n2)
+
+if operation == "%": 
+    n1 = int(input("enter first number: "))
+    n2 = int(input("enter second number: "))
+    print(n1 % n2)
+
+if operation == "*": 
+    n1 = int(input("enter first number: "))
+    n2 = int(input("enter second number: "))
+    print(n1 * n2)
+
+if operation == "**": 
+    n1 = int(input("enter first number: "))
+    n2 = int(input("enter second number: "))
+    print(n1 ** n2)
