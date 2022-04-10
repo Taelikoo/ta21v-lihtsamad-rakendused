d5b7cf0fe61934c4fdc235e4c526b43d9bf3b2f4
Taelikoo <taissiaa@mail.ru>
2022-04-10 19:46:43 +0300

    Update 1_calculator.py

--- 1_calculator.py
+++ 1_calculator.py
@@ -4,32 +4,32 @@ if operation == "+":
     n2 = int(input("enter second number: "))
     print(n1 + n2)
 
-if operation == "-": 
+elif operation == "-": 
     n1 = int(input("enter first number: "))
     n2 = int(input("enter second number: "))
     print(n1 - n2)
 
-if operation == "/": 
+elif operation == "/": 
     n1 = int(input("enter first number: "))
     n2 = int(input("enter second number: "))
     print(n1 / n2)
 
-if operation == "//": 
+elif operation == "//": 
     n1 = int(input("enter first number: "))
     n2 = int(input("enter second number: "))
     print(n1 // n2)
 
-if operation == "%": 
+elif operation == "%": 
     n1 = int(input("enter first number: "))
     n2 = int(input("enter second number: "))
     print(n1 % n2)
 
-if operation == "*": 
+elif operation == "*": 
     n1 = int(input("enter first number: "))
     n2 = int(input("enter second number: "))
     print(n1 * n2)
 
-if operation == "**": 
+elif operation == "**": 
     n1 = int(input("enter first number: "))
     n2 = int(input("enter second number: "))
     print(n1 ** n2)
